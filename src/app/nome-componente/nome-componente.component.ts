a21ec170f5d7e603f56e6d9b5a5e4f8ccadf7349
JuMedelha <dev.julliana@gmail.com>
2025-05-26 21:17:54 -0400

    3 commit

--- src/app/nome-componente/nome-componente.component.ts
+++ src/app/nome-componente/nome-componente.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nome-componente',
   imports: [],
+  standalone: true,
   templateUrl: './nome-componente.component.html',
   styleUrl: './nome-componente.component.css'
 })
 export class NomeComponenteComponent {
+  nome: string = "Julliana Medelha";
+  profissao:string = "Desenvolvedor Web";
+  hobbies: string[] = ["leitura", "Passear, Tecnologia", "Viajar"]
 
 }
---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  imports: [],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.css'
+})
+export class PerfilComponent {
+  perfil: {...} ={
+    username:'JullianaMedelha',
+    nome: 'Julliana Medelha',
+    local:'Manaus',
+    idade: 28,
+    profissao: 'Desenvolvedor Web',
+    empresa:'Sidia',
+    publicacoes:358 ,
+    seguidores: 666,
+    seguindo: 50,
+    foto: ,
+  };
+
+}
