e4a9d745db18a5e29e2e7c7b1de26dd921e406f2
JuMedelha <dev.julliana@gmail.com>
2025-05-26 20:32:53 -0400

    second commit

---
+++ src/app/nome-componente/nome-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-componente',
+  imports: [],
+  templateUrl: './nome-componente.component.html',
+  styleUrl: './nome-componente.component.css'
+})
+export class NomeComponenteComponent {
+
+}
